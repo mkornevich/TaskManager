2aa353b804ccdf10da1ada811eb7c76ed11679e7
mkornevich <mkornevich3@gmail.com>
2022-08-29 16:19:11 +0300

    init

---
+++ src/components/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+function App() {
+    let age =  '25';
+    return (
+        <div>{'Для того чтоб эта система заработала необходимо работать!'}</div>
+    );
+}
+export default App;
